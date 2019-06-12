05432e5191342c5c808fa73cb6485e3103a8c04a
moozygoo <39626225+moozygoo@users.noreply.github.com>
2019-06-12 12:36:46 +0200

    Change main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,5 @@
 //userek lekérése.
 jQuery.getJSON('users', function (users) {
     console.log('users', users);
-    if (true) {
-        console.log('ez a true');
-    }
-    //modositas
+    
 });
