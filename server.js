6f9390f6b366df2aca424188a50e7c3805562f94
moozygoo <39626225+moozygoo@users.noreply.github.com>
2019-06-12 16:41:58 +0200

    Present app.use

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
+// Beolvassuk a szükséges csomagokat.
 var express = require("express");
 var fs = require("fs");
+var itf = require('./my_modules/itf_module');
 
-var port=3500;
-var staticDir='build';
-//comment:)
+// Globális változók.
+var port = 3500;
+var staticDir = 'build/';
+
+// Létrehozunk egy express példányt.
 var app = express();
+
+// Statikus fájlok.
 app.use(express.static(staticDir));
+
+app.use(function (req, res, next) {
+    console.log(req.url);
+    next();
+});
+
+// Definiáljuk a szerver működését.
 app.get('/', function (req, res) {
     fs.readFile('./' + staticDir + '/index.html', 'utf8', function (err, data) {
         res.send(data);
     });
 });
 
+// Felhasználó modell.
 function handleUsers(req, res) {
     fs.readFile('./users.json', 'utf8', function (err, data) {
         if (err) throw err;
+
         //var path=req.url.split('/');
         var users = JSON.parse(data);
         var _user = {};
@@ -30,9 +45,12 @@ function handleUsers(req, res) {
         res.send(JSON.stringify(_user));
     });
 }
+// Felhasználók beolvasása.
 app.get('/users/:id*?', function (req, res) {
     console.log(req.url);
     handleUsers(req, res);
 });
+
+// Megadjuk, hogy a szerver melyik portot figyelje.
 app.listen(port);
-console.log("Server running in localhost:"+port);
+console.log("Server running in localhost:" + port);
